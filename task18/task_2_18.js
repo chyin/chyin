d628f1dc6666d0617da75179b2fcf7e80de7e452
code <chyin03@gmail.com>
2016-04-08 14:34:31 +0800

    task18 v1.0

---
+++ task18/task_2_18.js
@@ -0,0 +1,71 @@
+window.onload = function(){
+	var inNum = new Array();
+
+	document.getElementById("leftin").onclick = function(){
+		var num = document.getElementById("number").value;
+		if(isNaN(num)){
+			alert("Please input a number!")
+		}else{
+			inNum.length = inNum.length + 1;
+			for(var i=1;i<inNum.length;i++){
+				inNum[i] = inNum[i-1];
+			}
+			inNum[0] = num;
+			freshout();
+		}
+	};
+	document.getElementById("rightin").onclick = function(){
+		var num = document.getElementById("number").value;
+		if(isNaN(num)){
+			alert("Please input a number!")
+		}else{
+			inNum[inNum.length] = num;
+			freshout();
+		}
+	};
+	document.getElementById("leftout").onclick = function(){
+		for(var i=0;i<inNum.length-1;i++){
+			inNum[i] = inNum[i+1];
+		}
+		inNum.length = inNum.length - 1;
+		freshout();
+	};
+	document.getElementById("rightout").onclick = function(){
+		inNum.length = inNum.length - 1;
+		freshout();
+	};
+
+	function freshout(){
+		if(inNum.length === 0){
+			document.getElementById("divarea").innerHTML = "";
+		}
+		else{
+			divarea = document.getElementById("divarea");
+			divarea.innerHTML = "";
+			for(var i=0;i<inNum.length;i++){
+				var temptext;
+				temptext = "<div class='blockred' name = 'bred' id = "+i+">"+ inNum[i] +"</div>";
+				divarea.innerHTML = divarea.innerHTML + temptext;
+			}
+		}
+		var oNum = document.getElementsByName("bred");
+		console.log(oNum.length);
+		for(var i=0; i<oNum.length; i++){
+			oNum[i].onclick = function(){
+				console.log(this.id);
+				var deletId = parseInt(this.id);
+				for(var j=deletId;j<inNum.length-1;j++){
+					inNum[j] = inNum[j+1];
+				}
+				inNum.length = inNum.length - 1;
+				freshout();
+			}
+			oNum[i].onmouseover = function(){
+				this.style.cursor = "pointer";
+			}
+			oNum[i].onmouseout = function(){
+				this.style.cursor = "default";
+			}
+		}
+	}
+}
