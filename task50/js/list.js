3d4107e42aa935ba5808e1ed179490ecf31456a6
code <chyin03@gmail.com>
2016-05-08 01:14:58 +0800

    task50 v0.36

--- task50/js/list.js
+++ task50/js/list.js
@@ -1,11 +1,21 @@
+/*
 if(questionnaire.length===0){
+	console.log(11111);
 	$("#mbody").load("new.html");
 }
+*/
+/*
+var questionnaire = new Array();
+
+q1 = [12];
+questionnaire = [
+	{title:"我的问卷",time:new Date(2016,6,8),state:"1",question:q1}
+];
+*/
+
 var list = document.getElementById("list");
+var allCount = 0;
 tableAdd();
-document.getElementById("newButton2").onclick = function(){
-	$("#mbody").load("edit.html");
-}
 
 // 删除表格内容
 function tableDelete(){
@@ -18,29 +28,104 @@ function tableAdd(){
 	//console.log(questionnaire[0].state);
 	for(var i=0;i<questionnaire.length;i++){
 		var tempdata = "";
-		tempdata += "<td> <input type='checkbox' name=i/> </td>";
+		tempdata += "<tr class='trShow'><td> <input type='checkbox' name=" + i + " id=" + i + "> </td>";
+		questionnaire[i].check = 0;
 		tempdata += "<td>" + questionnaire[i].title + "</td>";
-		tempdata += "<td>" + questionnaire[i].time + "</td>";
+		var iTime = getTimeDetail(questionnaire[i].time);
+		tempdata += "<td>" + iTime.year + "-" + iTime.month + "-" + iTime.day + "</td>";
 		if(questionnaire[i].state===0){
-			tempdata += "<td> 未发布 </td>";
-			tempdata += "<td> <button>编辑</button><button>删除</button><button>查看问卷</button> </td>";
+			tempdata += "<td class='underPress'> 未发布 </td>";
+			tempdata += "<td> <button>编辑</button> <button>删除</button> <button>查看问卷</button> </td>";
 		}else{
 			var now = new Date();
-			if(now-questionnaire[i].time>0){
+			if(now-questionnaire[i].time<0){
 				tempdata += "<td> 已发布 </td>";
 			}else{
 				tempdata += "<td> 已结束 </td>";
 			}
-			tempdata += "<td> <button>查看数据</button><button>查看问卷</button> </td>";
+			tempdata += "<td> <button>查看数据</button> <button>查看问卷</button> </td>";
 		}
 
-		for(var j=0;j<questionnaire[i].length;j++){
-			tempdata += "<td>" + questionnaire[i][j] + "</td>";
-		}
-		list.innerHTML += tempdata;
+		list.innerHTML += tempdata + "</tr>";
 	}
-	tempdata = "<td colspan='5' align='left'> <input type='checkbox' name=all/> 全选 <button>删除</button> </td>";
+	tempdata = "<tr><td><input type='checkbox' name='all' id='all'/></td> <td colspan='4'>  <span class='deleteAll'>全选 <button>删除</button></span> </td></tr>";
 	list.innerHTML += tempdata;
-	//console.log(questionnaire);
-	//buttonReady();
+	tableShow();
+	buttonReady();
+}
+// 添加样式
+function tableShow() {
+	// 更改背景颜色
+	trShow = getClass('trShow');
+	for (var i = trShow.length - 1; i >= 0; i--) {
+		trShow[i].onmouseover = function(){
+			this.style.background = '#FCF0E5';
+		}
+		trShow[i].onmouseout = function(){
+			this.style.background = '#FFFFFF';
+		}
+	}
+}
+function buttonReady() {
+	document.getElementById("newButton2").onclick = function(){
+		$("#mbody").load("edit.html");
+	}
+	document.getElementById('all').onclick = function(){
+		if(allCount===0){
+			for (var i = questionnaire.length - 1; i >= 0; i--) {
+				document.getElementById(i).checked = true;
+				questionnaire[i].check = 1;
+				allCount = 1;
+			}
+		}else{
+			for (var i = questionnaire.length - 1; i >= 0; i--) {
+				document.getElementById(i).checked = false;
+				questionnaire[i].check = 0;
+				allCount = 0;
+			}
+		}
+	}
+	for (var checkedQ = questionnaire.length - 1; checkedQ >= 0; checkedQ--) {
+		document.getElementById(checkedQ).onclick = function(){
+			console.log(checkedQ);
+			questionnaire[checkedQ].check = 1 - questionnaire[checkedQ].check;
+			totalCount();
+		}
+	}
+}
+function totalCount() {
+	var tCount = 0;
+	for (var i = questionnaire.length - 1; i >= 0; i--) {
+		tCount += questionnaire[i];
+	}
+	if(tCount===questionnaire.length){
+		document.getElementById('all').checked = true;
+	}else{
+		document.getElementById('all').checked = false;
+	}
+}
+
+// 获取日期
+function getTimeDetail(timeX) {
+	var year = timeX.getFullYear();
+	var month = timeX.getMonth() + 1;
+	var day = timeX.getDate();
+	console.log(year,month,day);
+	return {year,month,day};
+}
+// getElementByClassName
+function getClass(clsName,parent){
+	var oParent = parent?document.getElementById(parent):document,
+		eles = [];
+		elements = oParent.getElementsByTagName('*');
+
+	for(var i=0,l=elements.length;i<l;i++){
+		classSplit = elements[i].className.split(new RegExp("\\s"));
+		for(var j=0;j<classSplit.length;j++){
+			if(classSplit[j]===clsName){
+				eles.push(elements[i]);
+			}
+		}
+	}
+	return eles;
 }
--- task50/js/task50.js
+++ task50/js/task50.js
@@ -1,9 +1,19 @@
+var questionnaire = new Array();
+
+q1 = [12];
+q2 = [123];
+q3 = [1234];
+d1 = [21];
+d2 = null;
+d3 = [4321];
+questionnaire = [
+	{title:"我的问卷1",time:new Date(2015,8,8),state:1,question:q1,data:d1},
+	{title:"我的问卷2",time:new Date(2016,5,8),state:0,question:q2,data:d2},
+	{title:"我的问卷3",time:new Date(2016,8,8),state:1,question:q3,data:d3}
+];
+
+
 window.onload = function(){
-	var questionnaire = new Array();
-	q1 = [12];
-	questionnaire = [
-		//{title:"我的问卷",time:new Date(),state:"1",question:q1}
-	];
 	//console.log(questionnaire);
 	
 	//链接事件
@@ -25,12 +35,16 @@ window.onload = function(){
 	$("#mbody").load("list.html");
 	//加载list或new
 	choose();
-	
+
 	function choose(){
 		if(questionnaire.length === 0){
 			$("#mbody").load("new.html");
 		}else{
-			$("#mbody").load("list.html");
+			if(questionnaire.length > 0){
+				$("#mbody").load("list.html");
+			}else{
+				console.log("Something wrong with questionnaire!");
+			}
 		}
 	}
 }
